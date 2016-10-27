b6b68c90e630df9904a48f75623675f01297ddbe
songdezhong <songdezhong@58ganji.com>
2016-10-27 17:12:20 +0800

    项目初始化

---
+++ ForMeal/ForMeal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ForMeal
+//
+//  Created by song_dzhong on 16/10/27.
+//  Copyright © 2016年 songdezhong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
